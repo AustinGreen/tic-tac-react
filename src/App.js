5d840562d80b4d12737346a6845875d625241fd9
Austin Green <austinzgreen@gmail.com>
2017-03-22 18:11:05 -0400

    winner logic is done, next up you need to end the game

--- src/App.js
+++ src/App.js
@@ -120,17 +120,20 @@ class App extends Component {
     if (mostRecentSquare.row === 0 && mostRecentSquare.col === 0) {
       if (game[0][1] === game[0][2]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][1]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[1][0] === game[2][0]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[1][0]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[1][1] === game[2][2]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[1][1]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
     }
@@ -138,12 +141,14 @@ class App extends Component {
     if (mostRecentSquare.row === 0 && mostRecentSquare.col === 1) {
       if (game[0][0] === game[0][2]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][0]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[1][1] === game[2][1]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[1][1]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
     }
@@ -151,17 +156,20 @@ class App extends Component {
     if (mostRecentSquare.row === 0 && mostRecentSquare.col === 2) {
       if (game[0][0] === game[0][1]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][0]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[1][2] === game[2][2]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[1][2]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[1][1] === game[2][0]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[1][1]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
     }
@@ -169,12 +177,14 @@ class App extends Component {
     if (mostRecentSquare.row === 1 && mostRecentSquare.col === 0) {
       if (game[1][1] === game[1][2]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[1][1]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[0][0] === game[2][0]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][0]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
     }
@@ -182,22 +192,26 @@ class App extends Component {
     if (mostRecentSquare.row === 1 && mostRecentSquare.col === 1) {
       if (game[1][0] === game[1][2]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[1][0]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[0][1] === game[2][1]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][1]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[0][0] === game[2][2]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][0]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[0][2] === game[2][0]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][2]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
     }
@@ -205,58 +219,68 @@ class App extends Component {
     if (mostRecentSquare.row === 1 && mostRecentSquare.col === 2) {
       if (game[1][0] === game[1][1]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[1][0]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[0][2] === game[2][2]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][2]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
     }
     if (mostRecentSquare.row === 2 && mostRecentSquare.col === 0) {
       if (game[2][1] === game[2][2]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[2][1]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[0][0] === game[1][0]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][0]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[0][2] === game[1][1]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][2]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
     }
     if (mostRecentSquare.row === 2 && mostRecentSquare.col === 1) {
       if (game[2][0] === game[2][2]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[2][0]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[0][1] === game[1][1]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][1]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
     }
     if (mostRecentSquare.row === 2 && mostRecentSquare.col === 2) {
       if (game[2][0] === game[2][1]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[2][0]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[0][2] === game[1][2]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][2]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
       if (game[0][0] === game[1][1]) {
         if (game[mostRecentSquare.row][mostRecentSquare.col] === game[0][0]) {
-          this.endGame();
+          let winner = currentTurn === 'X' ? 'O' : 'X';
+          this.endGame(winner);
         }
       }
     }
@@ -284,8 +308,8 @@ class App extends Component {
     });
   }
 
-  endGame() {
-    console.log('Game over!');
+  endGame(winner) {
+    console.log(`Congratulations Player ${winner}! You won.`);
   }
 
   render() {
