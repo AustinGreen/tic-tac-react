2f1f92fa79d6a6995a1fc8966fc1787401a9e98c
Austin Green <austinzgreen@gmail.com>
2017-03-22 10:55:38 -0400

    scoreboard logic

--- src/App.js
+++ src/App.js
@@ -1,63 +1,72 @@
 import React, { Component } from 'react';
 
-const Board = ({ onClick }) => (
+const Board = ({ onClick, gameState }) => (
   <div>
     <div className="columns is-mobile">
       <div
         className="column is-4 Board__square"
         style={{ borderWidth: '0 1px 1px 0' }}
-        onClick={onClick}
+        onClick={() => onClick(0, 0)}
       >
-        <p className="title has-text-centered">{display}</p>
+        <p className="title has-text-centered">{gameState[0][0]}</p>
       </div>
       <div
-        className="column is-4 Board__square has-text-centered"
+        className="column is-4 Board__square"
         style={{ borderWidth: '0 1px 1px 0' }}
+        onClick={() => onClick(0, 1)}
       >
-        <a className="title" onClick={() => console.log('X')}>X</a>
+        <p className="title has-text-centered">{gameState[0][1]}</p>
       </div>
       <div
-        className="column is-4 Board__square has-text-centered"
+        className="column is-4 Board__square"
         style={{ borderWidth: '0 0 1px 0' }}
+        onClick={() => onClick(0, 2)}
       >
-        <a className="title" onClick={() => console.log('X')}>O</a>
+        <p className="title has-text-centered">{gameState[0][2]}</p>
       </div>
     </div>
     <div className="columns is-mobile">
       <div
-        className="column is-4 Board__square has-text-centered"
+        className="column is-4 Board__square"
         style={{ borderWidth: '0 1px 1px 0' }}
+        onClick={() => onClick(1, 0)}
       >
-        <a className="title" onClick={() => console.log('X')}>X</a>
+        <p className="title has-text-centered">
+          {gameState[1][0]}
+        </p>
       </div>
       <div
-        className="column is-4 Board__square has-text-centered"
+        className="column is-4 Board__square"
         style={{ borderWidth: '0 1px 1px 0' }}
+        onClick={() => onClick(1, 1)}
       >
-        <a className="title" onClick={() => console.log('X')}>O</a>
+        <p className="title has-text-centered">{gameState[1][1]}</p>
       </div>
       <div
-        className="column is-4 Board__square has-text-centered"
+        className="column is-4 Board__square"
         style={{ borderWidth: '0 0 1px 0' }}
+        onClick={() => onClick(1, 2)}
       >
-        <a className="title" onClick={() => console.log('X')}>X</a>
+        <p className="title has-text-centered">{gameState[1][2]}</p>
       </div>
     </div>
     <div className="columns is-mobile">
       <div
-        className="column is-4 Board__square has-text-centered"
+        className="column is-4 Board__square"
         style={{ borderWidth: '0 1px 0 0' }}
+        onClick={() => onClick(2, 0)}
       >
-        <a className="title" onClick={() => console.log('X')}>O</a>
+        <p className="title has-text-centered">{gameState[2][0]}</p>
       </div>
       <div
-        className="column is-4 Board__square has-text-centered"
+        className="column is-4 Board__square"
         style={{ borderWidth: '0 1px 0 0' }}
+        onClick={() => onClick(2, 1)}
       >
-        <a className="title" onClick={() => console.log('X')}>X</a>
+        <p className="title has-text-centered">{gameState[2][1]}</p>
       </div>
-      <div className="column is-4 Board__square has-text-centered">
-        <a className="title" onClick={() => console.log('X')}>X</a>
+      <div className="column is-4 Board__square" onClick={() => onClick(2, 2)}>
+        <p className="title has-text-centered">{gameState[2][2]}</p>
       </div>
     </div>
   </div>
@@ -67,20 +76,33 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      game: [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']],
+      game: [['', '', ''], ['', '', ''], ['', '', '']],
+      currentTurn: 'X',
     };
 
     // Bind class methods to object instances
-    // this.updateBoard = this.updateBoard.bind(this);
+    this.updateBoard = this.updateBoard.bind(this);
   }
 
-  updateBoard() {}
+  updateBoard(row, col) {
+    const { game, currentTurn } = this.state;
+    if (game[row][col].length) return;
+    game[row][col] = currentTurn;
+    this.setState({
+      game,
+      currentTurn: currentTurn === 'X' ? 'O' : 'X',
+    });
+    console.log(game);
+  }
 
   render() {
+    const {
+      game,
+    } = this.state;
     return (
       <div>
         <div className="section column is-half is-offset-one-quarter">
-          <Board onClick={() => this.updateBoard()} />
+          <Board gameState={game} onClick={(row, col) => this.updateBoard(row, col)} />
         </div>
       </div>
     );
